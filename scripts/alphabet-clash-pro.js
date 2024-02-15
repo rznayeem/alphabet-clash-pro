1876d42a7ef92fb651335dabd6c9a570e133db61
rznayeem <riyazulnayeem@gmail.com>
2024-02-16 02:40:39 +0600

    final

--- scripts/alphabet-clash-pro.js
+++ scripts/alphabet-clash-pro.js
@@ -1,6 +1,11 @@
 function play() {
   const hideElement = hideElementById('home');
+  const hideElement2 = hideElementById('play-again');
   const showElement = showElementById('game-screen');
+  const defaultLife = document.getElementById('current-life');
+  defaultLife.innerText = 3;
+  const defaultScore = document.getElementById('current-score');
+  defaultScore.innerText = 0;
   continueGame();
 }
 function continueGame() {
@@ -11,6 +16,10 @@ function continueGame() {
 }
 function handelKeyPress(event) {
   const playerPressed = event.key;
+  if (playerPressed === 'Escape') {
+    gameOver();
+    removeBgById(expectedKey);
+  }
   const expectedKeyElement =
     document.getElementById('game-screen-input').innerText;
   const expectedKey = expectedKeyElement.toLowerCase();
@@ -27,9 +36,15 @@ function handelKeyPress(event) {
     const newLifeScore = lifeScore - 1;
     defaultLife.innerText = newLifeScore;
     if (newLifeScore === 0) {
-      hideElementById('game-screen');
-      showElementById('play-again');
+      gameOver();
+      removeBgById(expectedKey);
     }
   }
 }
 document.addEventListener('keyup', handelKeyPress);
+function gameOver() {
+  hideElementById('game-screen');
+  showElementById('play-again');
+  document.getElementById('final-score').innerText =
+    document.getElementById('current-score').innerText;
+}
